ab4f27c9f68834d00fb85c20bbebddcb531331da
betul <bsenger@aof.anadolu.edu.tr>
2023-03-19 15:51:36 +0300

    feat : template fixed

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum sunt voluptatem voluptas numquam ut facilis? Facere atque itaque, repellendus, modi,
+     vitae neque similique illum molestiae laudantium inventore a sed laboriosam?</div>
+  );
+
+
